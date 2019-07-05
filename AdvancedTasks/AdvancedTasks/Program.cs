6fd55f4623d8e60d68d35b99af09d3e0873ae2dd
Ming Zhu <ming.zhu@gov.ab.ca>
2019-07-05 17:28:37 -0600

    Threadpool

---
+++ AdvancedTasks/AdvancedTasks/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdvancedTasks
+{
+    class Program
+    {
+        public static int counter;
+
+        public static void Main()
+        {
+            ThreadPool.QueueUserWorkItem((s) =>
+            {
+                Console.WriteLine("Working on a thread from threadpool right here..");
+            });
+
+            Console.WriteLine("Press any key to exist");
+            Console.ReadLine();
+        }
+    }
+}
